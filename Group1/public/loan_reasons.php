e34707cdd85ba0ac0d5cd76de4f9a79973507496
ALEMI BLESS <alemibless@gmail.com>
2023-08-24 12:43:41 +0300

    Loans updates

--- Group1/public/loan_reasons.php
+++ Group1/public/loan_reasons.php
@@ -18,7 +18,7 @@
         </div>
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Rejected Loan Reasons</h3>
+                <h3 class="card-title"> Rejected Loan Reasons </h3>
             </div>
             <div class="card-body">
                 <table class="table table-bordered" id="loan-list">
